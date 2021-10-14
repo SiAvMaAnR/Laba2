3d4b4cdd4e25e0b37c4129c9954c407b11a24e1c
SiAvMaAnR <samarkin20022002@gmail.com>
2021-10-14 21:55:39 +0300

    14.10.21 21.55

---
+++ SecondLaba/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SecondLaba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
