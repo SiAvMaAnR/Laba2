f621e3bdd2fc0368731a8f0dd3e08954ab736a21
SiAvMaAnR <samarkin20022002@gmail.com>
2021-10-15 22:58:16 +0300

    15.10.21

--- SecondLaba/Program.cs
+++ SecondLaba/Program.cs
@@ -1,57 +1,138 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace SecondLaba
 {
     class Program
     {
+        static object locker = new object();
+        static Random random = new Random();
+
         static void Main(string[] args)
         {
-            string text = Console.ReadLine();
+            //string text = Console.ReadLine();
+            //Console.WriteLine(levenshtein(text, pattern));
+
+            string text = new string(GenerationString(8, 12));
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(text);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("Введите искомое словосочетание: ");
             string pattern = Console.ReadLine();
 
-            Console.WriteLine(levenshtein(text, pattern));
-            Console.ReadKey(true);
+            Console.WriteLine(MethodWF(text, pattern,1,1,2));
+            Console.ReadKey();
         }
 
-
-        static int levenshtein(string a, string b)
+        //Метод Вагнера-Фишера
+        static int MethodWF(string s1, string s2, int InsertCost = 1, int DeleteCost = 1, int ReplaceCost = 1)
         {
-            if (string.IsNullOrEmpty(a))
+            if (string.IsNullOrEmpty(s1))
             {
-                return (!string.IsNullOrEmpty(b)) ? b.Length : 0;
+                return (!string.IsNullOrEmpty(s2)) ? s2.Length : 0;
             }
-            if (string.IsNullOrEmpty(b))
+            if (string.IsNullOrEmpty(s2))
             {
-                return (!string.IsNullOrEmpty(a)) ? a.Length : 0;
+                return (!string.IsNullOrEmpty(s1)) ? s1.Length : 0;
             }
 
-            int[,] d = new int[a.Length + 1, b.Length + 1];
+            int[,] D = new int[s1.Length + 1, s2.Length + 1];
+            int M = D.GetUpperBound(0);
+            int N = D.GetUpperBound(1);
+            D[0, 0] = 0;
+            for (int i = 1; i <= M; i++)
+            {
+                D[i, 0] = D[i - 1, 0] + DeleteCost;
+            }
+            Console.WriteLine();
+
+            for (int j = 1; j <= N; j++)
+            {
+                D[0, j] = D[0, j - 1] + InsertCost;
+            }
+            Console.WriteLine();
+
+            for (int i = 1; i <= M; i++)
+            {
+                for (int j = 1; j <= N; j++)
+                {
+                    if (s1[i-1] != s2[j-1])
+                        D[i, j] = Math.Min(Math.Min(D[i - 1, j] + DeleteCost, D[i, j - 1] + InsertCost), D[i - 1, j - 1] + ReplaceCost);
+                    else
+                        D[i, j] = D[i - 1, j - 1];
+                }
+            }
+            return D[M, N];
+        }
+
+        static char[] GenerationString(int n, int m, char[] text = default, string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz ")
+        {
+            if (alphabet == null)
+            {
+                alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz ";
+            }
+
+            int length = random.Next(n, m + 1);
+
+            if (text == null)
+            {
+                text = new char[length];
+                if (Parallel.For(0, length, x =>
+                {
+                    int index;
+                    lock (locker)
+                    {
+                        index = random.Next(0, alphabet.Length);
+                    }
+                    text[x] = alphabet[index];
+                }).IsCompleted)
+                {
+                    return text;
+                }
+            }
+            return text;
+        }
+        #region
+        static int levenshtein(string s1, string s2)
+        {
+            if (string.IsNullOrEmpty(s1))
+            {
+                return (!string.IsNullOrEmpty(s2)) ? s2.Length : 0;
+            }
+            if (string.IsNullOrEmpty(s2))
+            {
+                return (!string.IsNullOrEmpty(s1)) ? s1.Length : 0;
+            }
+
+            int[,] D = new int[s1.Length + 1, s2.Length + 1];
             int cost, min1, min2, min3;
 
-            int N = d.GetUpperBound(0);
-            int M = d.GetUpperBound(1);
+            int N = D.GetUpperBound(0);
+            int M = D.GetUpperBound(1);
 
             for (int i = 0; i <= N; i++)
             {
-                d[i, 0] = i;
+                D[i, 0] = i;
             }
             for (int i = 0; i <= M; i++)
             {
-                d[0, i] = i;
+                D[0, i] = i;
             }
             for (int i = 1; i <= N; i++)
             {
                 for (int j = 1; j <= M; j++)
                 {
-                    cost = Convert.ToInt32(!(a[i - 1] == b[j - 1]));
+                    cost = Convert.ToInt32((s1[i - 1] != s2[j - 1]));
 
-                    min1 = d[i - 1, j] + 1;
-                    min2 = d[i, j - 1] + 1;
-                    min3 = d[i - 1, j - 1] + cost;
-                    d[i, j] = Math.Min(Math.Min(min1, min2), min3);
+                    min1 = D[i - 1, j] + 1;
+                    min2 = D[i, j - 1] + 1;
+                    min3 = D[i - 1, j - 1] + cost;
+                    D[i, j] = Math.Min(Math.Min(min1, min2), min3);
                 }
             }
-            return d[N, M];
+            return D[N, M];
         }
+        #endregion
     }
 }
