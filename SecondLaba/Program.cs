dfdeb90b174f676d74eb20435fb68c5e815ba4a1
SiAvMaAnR <samarkin20022002@gmail.com>
2021-10-15 15:21:44 +0300

    15.10.21 15.21

--- SecondLaba/Program.cs
+++ SecondLaba/Program.cs
@@ -6,7 +6,12 @@ namespace SecondLaba
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(levenshtein("дагестан", "арестант"));
+            string text = Console.ReadLine();
+            string pattern = Console.ReadLine();
+
+
+
+            Console.WriteLine(levenshtein(text, pattern));
         }
 
 
@@ -16,27 +21,28 @@ namespace SecondLaba
             {
                 return (!string.IsNullOrEmpty(b)) ? b.Length : 0;
             }
-
             if (string.IsNullOrEmpty(b))
             {
-                return (!string.IsNullOrEmpty(a)) ?a.Length:0;
+                return (!string.IsNullOrEmpty(a)) ? a.Length : 0;
             }
-            int cost;
+
             int[,] d = new int[a.Length + 1, b.Length + 1];
-            int min1;
-            int min2;
-            int min3;
-            for (int i = 0; i <= d.GetUpperBound(0); i++)
+            int cost, min1, min2, min3;
+
+            int N = d.GetUpperBound(0);
+            int M = d.GetUpperBound(1);
+
+            for (int i = 0; i <= N; i++)
             {
                 d[i, 0] = i;
             }
-            for (int i = 0; i <= d.GetUpperBound(1); i++)
+            for (int i = 0; i <= M; i++)
             {
                 d[0, i] = i;
             }
-            for (int i = 1; i <= d.GetUpperBound(0); i++)
+            for (int i = 1; i <= N; i++)
             {
-                for (int j = 1; j <= d.GetUpperBound(1); j++)
+                for (int j = 1; j <= M; j++)
                 {
                     cost = Convert.ToInt32(!(a[i - 1] == b[j - 1]));
 
@@ -46,7 +52,7 @@ namespace SecondLaba
                     d[i, j] = Math.Min(Math.Min(min1, min2), min3);
                 }
             }
-            return d[d.GetUpperBound(0), d.GetUpperBound(1)];
+            return d[N, M];
         }
     }
 }
